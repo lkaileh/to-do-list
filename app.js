23082876abac530356ab3329d24227614f1c2463
Lou Kaileh <lkaileh@gmail.com>
2017-03-13 16:31:42 -0700

    Removed index.js and replaced it with app.js.  Also added some functionality between index.html and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('ngTodos', [])
+.controller('todoController', function($scope){
+  $scope.todos = [];
+
+  $scope.addTodo = function () {
+    $scope.todos.push($scope.newTodo);
+    $scope.newTodo = '';
+  };
+
+  $scope.removeTodo = function(index) {
+    $scope.todos.splice(index, 1);
+  };
+});
+//.factory ('todoData')
