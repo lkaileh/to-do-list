4c2bd29b868d00b9bf6e50c9762a319c7c9ec679
Lou Kaileh <lkaileh@gmail.com>
2017-03-13 17:32:32 -0700

    fixed bug in server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/'));
 
 app.get('/', function(request, response) {
-  response.render('app');
+
 });
 
-app.lisgen(app.get('port'), function() {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port ', app.get('port'));
 });
