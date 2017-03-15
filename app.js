1c13ef8411db354b51512f4f5df125d5b464a2c2
Lou Kaileh <lkaileh@gmail.com>
2017-03-14 19:24:26 -0700

    functioning post to DB, but not passing input variable yet

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
 
 
 angular.module('ngTodos', [])
-.controller('todoController', function($scope) {
+.controller('todoController', function($scope, $http) {
+  $scope.getAll = function() {
+    $http.get('/', $scope)
+    .success(function(data) {
+      console.log("posted successfully");
+    }).error(function(data) {
+      console.error("error in posting");
+    });
+  };  
   $scope.todos = [];
+  $scope.todos.push($scope.getAll);
 
   $scope.addTodo = function () {
-    $scope.todos.push($scope.newTodo);
-    $scope.newTodo = '';
-  };
+    $http.post('/', $scope)
+    .success(function(data) {
+      console.log("posted successfully" + $scope.newTodo);
+    }).error(function(data) {
+      console.error("error in posting");
+    });
+  }; 
+    //$scope.todos.push($scope.newTodo);
+    //$scope.todos = app.post($scope.newTodo);
+    //console.log(app);
+  $scope.todos.push($scope.getAll());
+  $scope.newTodo = '';
+  console.log($scope.todos);
 
   $scope.removeTodo = function(index) {
     $scope.todos.splice(index, 1);
+    console.log("removeTodo");
   };
-});
-//.factory ('todoData')
+});  
+//};
+
+// angular.module('Todos', [])
+// .factory ('todoData', function($http) {
+//   var getAll = function () {
+//     return $http({
+//       method: 'GET',
+//       url: '/',
+//       success: function () {
+//         console.log('google');
+//       }  
+//     }).then(function (resp) {
+//       return resp.data;
+//     });
+//   };
+// }); 
--- server.js
+++ server.js
@@ -1,20 +1,67 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 
 app.use(express.static(__dirname));
 
-var db_config = {
-    host: 'us-cdbr-iron-east-03.cleardb.net',
-    user: 'b4a3bc0c9cb84f',
-    password: '9397b521',
-    database: 'heroku_5479819faf5a4e1'
-};
 
- //app.get('/', function(request, response) {
-   //response.render('app');
- //});
+ 
+
+// app.get('/', function(request, response, next) {
+//   console.log('this is a test');
+//   next();
+// });
+ 
+
+var dbConfig = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'todolist'  
+}; 
+
+var connection;
+
+connection = mysql.createConnection(dbConfig);
+
+connection.connect(function(err) {                // The server is either down
+  if (err) {                                     // or restarting (takes a while sometimes).
+    console.log('2. error when connecting to db:', err);
+    setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+  }                                       // to avoid a hot loop, and to allow our node script to
+});                                       // process asynchronous requests in the meantime.
+                          // If you're also serving http, display a 503 error.
+connection.on('error', function(err) {
+  console.log('3. db error', err);
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {  // Connection to the MySQL server is usually
+    handleDisconnect();                       // lost due to either server restart, or a
+  } else {                                        // connnection idle timeout (the wait_timeout
+    throw err;                                  // server variable configures this)
+  }
+});
+
+var query = connection.query('SELECT item FROM list');
+ // app.get('/', function(request, response) {
+ //   response.render('app');
+ // });
+ 
+var insert = function(body) {
+  connection.query('INSERT INTO list (`item`) VALUES(" + variable + ");');
+}; 
+
+app.get('/', function(req, res) {
+  return res.send(query);
+});
+
+app.post('/', function(req, res) {
+  insert(req.data);
+}); 
+ 
+// app.get('*', function (req, res) {
+//   res.sendFile('/public/index.html');
+// }); 
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port ', app.get('port'));
